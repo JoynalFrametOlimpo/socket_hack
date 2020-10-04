5c2015cdc01a0235269ec532853a78463c1be1ab
Dirtic <desarrollo@armada.mil.ec>
2020-10-04 07:26:27 -0500

    Allow input text for analitics

--- tcp_client.py
+++ tcp_client.py
@@ -1,18 +1,20 @@
 import socket
 import sys
 
-target_host = "www.google.com"
-target_port = 80
+target_host = str(input('Taget host: '))
+target_port = int(input('Taget port: '))
+
 
 #create a socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-
 #connect the client
-client.connect((target_host,target_port))
+client.connect((str(target_host),int(target_port)))
 
 #send some data
-client.send(b'GET /HTTP/1.1\r\nHost: google.com\r\n\r\n')
+#client.send(b'GET /HTTP/1.1\r\nHost: google.com\r\n\r\n')
+message = str.encode((input('Enter your message: ')))
+client.send(message)
 
 # receive some data
 response = client.recv(4096)
